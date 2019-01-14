8172461f4b15336efb20e034740d1b6325a1fbbc
Andres Picazo Cuesta <apycazo@gmail.com>
2019-01-14 23:15:42 +0100

    added example using bom

---
+++ launchpad-sandbox/src/main/java/es/jander/launchbase/sandbox/SandboxApp.java
@@ -0,0 +1,11 @@
+package es.jander.launchbase.sandbox;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SandboxApp {
+
+  public static void main(String[] args) {
+
+  }
+}
